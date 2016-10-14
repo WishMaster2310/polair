f6080ccb9855b4335b4cee7270b97931611c01f6
Vova <wishmaster2310@gmail.com>
2016-10-14 22:20:25 +0500

    up

---
+++ public/javascripts/app.js
@@ -0,0 +1,8 @@
+$(function() {
+	$('.c-hero__slider').slick({
+		prevArrow: '<div class="c-hero__arrow c-hero__arrow--prev"></div>',
+		nextArrow: '<div class="c-hero__arrow c-hero__arrow--next"></div>',
+		dots: true,
+		infinite: false
+	});
+});
